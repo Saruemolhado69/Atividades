789ed0dd6bf6c1bd120f9cc78bc084084dddba55
Saruemolhado69 <saruemolhado69@gmail.com>
2024-05-16 14:08:53 -0300

    Add files via upload

---
+++ At2 Cálculos com números.py
@@ -0,0 +1,8 @@
+numero = 1
+
+while numero > 0:
+    soma = 1
+    numero = int(input('Informe o número: (0 para sair):'))
+    if numero > 1:
+        for i in range(1, numero):
+            if i % 2 == 
